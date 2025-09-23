2c1af83592c6b5ac1f6dd465ccf834c0a393493c
emrecetinkaya <emrecetinkayadev@gmail.com>
2025-09-24 00:25:13 +0300

    feat(node-agent): wire metrics server into main runtime

--- node-agent/cmd/agent/main.go
+++ node-agent/cmd/agent/main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/emrecetinkayadev/vpn-tridot/node-agent/internal/config"
+)
+
 func main() {
-	// TODO: start node agent runtime.
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+	}
+
+	agent, exporter, err := newAgent(cfg)
+	if err != nil {
+		log.Fatalf("init agent: %v", err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+	var metricsSrv *http.Server
+	var metricsDone chan struct{}
+	if exporter != nil && cfg.Agent.MetricsAddress != "" {
+		metricsSrv = &http.Server{Addr: cfg.Agent.MetricsAddress, Handler: exporter.Handler()}
+		metricsDone = make(chan struct{})
+		go func() {
+			defer close(metricsDone)
+			if err := metricsSrv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				log.Printf("metrics server error: %v", err)
+			}
+		}()
+		go func() {
+			<-ctx.Done()
+			shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			if err := metricsSrv.Shutdown(shutdownCtx); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				log.Printf("metrics server shutdown: %v", err)
+			}
+		}()
+	}
+
+	if err := agent.Run(ctx); err != nil {
+		log.Fatalf("agent error: %v", err)
+	}
+	stop()
+	if metricsDone != nil {
+		<-metricsDone
+	}
 }
